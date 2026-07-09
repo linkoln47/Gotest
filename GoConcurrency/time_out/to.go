8d0781f09d054665da00b031f7f4683c22ea073d
linkoln47 <fedunov1995@gmail.com>
2026-07-09 16:21:52 +0900

    time_out in concurrency

---
+++ GoConcurrency/time_out/to.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	result, err := timeLimit(doSomeWork, 2*time.Second)
+	fmt.Println(result, err)
+}
+
+func timeLimit[T any](worker func() T, limit time.Duration) (T, error) {
+	out := make(chan T, 1)
+	ctx, cancel := context.WithTimeout(context.Background(), limit)
+	defer cancel()
+	go func() {
+		out <- worker()
+	}()
+	select {
+	case result := <-out:
+		return result, nil
+	case <-ctx.Done():
+		var zero T
+		return zero, errors.New("work times out")
+	}
+}
+
+func doSomeWork() int {
+	if x := rand.Int(); x%2 == 0 {
+		return x
+	} else {
+		time.Sleep(10 * time.Second)
+		return 100
+	}
+}
